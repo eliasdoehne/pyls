31c591f106c2cc631780a23836ece66f96662ea4
eliasdoehne <elias.doehne@hemio.de>
2018-11-18 16:03:43 +0100

    Basic project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pyls",
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "pyls = pyls.pyls:main",
+        ],
+    },
+)
